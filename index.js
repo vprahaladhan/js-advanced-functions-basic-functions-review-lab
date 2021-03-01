d5e34c9c3f9b4cbec51242599c5e5d224a31275b
prahal <vprahaladhan@gmail.com>
2021-03-01 20:06:53 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+const saturdayFun = (activity = 'roller-skate') => `This Saturday, I want to ${activity}!`;
+
+const mondayWork = (activity = 'go to the office') => `This Monday, I will ${activity}.`;
+
+const wrapAdjective = (highlighter = '*') => {
+  return (adjective) => `You are ${highlighter}${adjective}${highlighter}!`;
+}
+
+let Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b
+};
+
+const actionApplyer = (baseNum, arrayOfTransforms) => {
+  if (arrayOfTransforms.length === 0) {
+    return baseNum;
+  }
+
+  for (const transform of arrayOfTransforms) {
+    baseNum = transform(baseNum);
+  }
+  return baseNum;
+}
